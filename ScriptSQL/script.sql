4f043817617170dc8f5a4bc6e4e076312eb167bb
radiaide <idelkadiradia@gmail.com>
2024-01-07 12:21:07 +0100

    Conception UML /DB

---
+++ ScriptSQL/script.sql
@@ -0,0 +1,39 @@
+create DATABASE wiki_platform_db;
+use wiki_platform_db;
+CREATE TABLE Users (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255),
+    role VARCHAR(6)
+);
+CREATE TABLE Categories (
+    category_id INT PRIMARY KEY AUTO_INCREMENT,
+    category_name VARCHAR(255)
+);
+CREATE TABLE Tags (
+    tag_id INT PRIMARY KEY AUTO_INCREMENT,
+    tag_name VARCHAR(255),
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id) ON DELETE CASCADE
+);
+
+CREATE TABLE Wikis (
+    wiki_id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(255),
+    content TEXT,
+    author_id INT,
+    category_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    archived BOOLEAN DEFAULT 0, -- Indique si le wiki est archivé
+    FOREIGN KEY (author_id) REFERENCES Users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id) ON DELETE CASCADE
+);
+CREATE TABLE WikiTags (
+    wiki_id INT,
+    tag_id INT,
+    PRIMARY KEY (wiki_id, tag_id),
+    FOREIGN KEY (wiki_id) REFERENCES Wikis(wiki_id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES Tags(tag_id) ON DELETE CASCADE
+);

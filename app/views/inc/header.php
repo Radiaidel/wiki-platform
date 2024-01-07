aadaf5b09fe7c5b1b7acbab868f70b100e126298
radiaide <idelkadiradia@gmail.com>
2024-01-07 21:46:45 +0100

    new style for header

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -19,16 +19,16 @@
                 <div class="flex items-center">
                     <div class="w-12 h-12">
                         <div class="w-full text-center ">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png" 
-                            class=" responsive-image"  alt="Logo" >
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/80/Wikipedia-logo-v2.svg/1200px-Wikipedia-logo-v2.svg.png"
+                                class=" responsive-image" alt="Logo">
                         </div>
                     </div>
                     <div class="relative sm:flex items-center ml-10">
                         <!-- Removed sm:hidden to keep the search bar always visible -->
                         <input type="text" placeholder=""
                             class="h-10 w-48 sm:w-96 px-4 text-sm text-white placeholder-white bg-gray-700 rounded-full focus:outline-none focus:shadow-none">
-                        <svg class="absolute right-3 top-3 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24" stroke="currentColor">
+                        <svg class="absolute right-3 top-3 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                         </svg>
@@ -36,7 +36,7 @@
 
 
 
-                   
+
                 </div>
                 <div class="flex items-center ml-4 md:ml-6">
                     <div class="relative ml-3">
@@ -44,8 +44,8 @@
                             <button
                                 class="flex items-center max-w-xs text-sm text-white rounded-full focus:outline-none focus:shadow-solid"
                                 id="user-menu" aria-label="User menu" aria-haspopup="true" onclick="toggleDropdown()">
-                                <img class="w-12 h-12 rounded-full"
-                                    src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFile%3ADefault_pfp.svg&psig=AOvVaw062NkXsm1clUSWuyBvjF2L&ust=1704742653940000&source=images&cd=vfe&ved=0CBEQjRxqFwoTCPj-xLWDzIMDFQAAAAAdAAAAABAE"
+                                <img  class="w-12 h-12 rounded-full"
+                                    src="<?php echo isset($_SESSION['userprofile']) ? URLROOT . '/public/'.$_SESSION["userprofile"]: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg'; ?>"
                                     alt="User" />
                                 <svg width="30px" height="30px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"
                                     fill="#c0c0c0">
@@ -64,10 +64,22 @@
                             <!-- Dropdown content -->
                             <div id="dropdown-content"
                                 class="hidden group-hover:block absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg z-10">
-                                <?php if (isset($_SESSION['user_id'])): ?>
+                                <?php if (isset($_SESSION['user_role']) && $_SESSION['user_role'] == "admin"): ?>
+                                    <a href="<?php echo URLROOT; ?>/Pages/dashboard"
+                                        class="block hover:bg-gray-100 px-4 py-2 text-sm text-gray-700">Dashboard</a>
+                                        <a href="<?php echo URLROOT; ?>/Pages/categorie"
+                                        class="block hover:bg-gray-100 px-4 py-2 text-sm text-gray-700">Categories</a>
+                                        <a href="<?php echo URLROOT; ?>/Pages/tag"
+                                        class="block hover:bg-gray-100 px-4 py-2 text-sm text-gray-700">Tags</a>
+
+                                <a href="<?php echo URLROOT; ?>/Pages/index"
+                                    class="block hover:bg-gray-100 px-4 py-2 text-sm text-gray-700">Accueil</a>
+
+                                <?php elseif (isset($_SESSION['user_role']) && $_SESSION['user_role'] == "auteur"): ?>
                                     <a href="#" class="block hover:bg-gray-100 px-4 py-2 text-sm text-gray-700">Add new
                                         wiki</a>
                                     <a href="#" class="block hover:bg-gray-100 px-4 py-2 text-sm text-gray-700">Log out</a>
+
                                 <?php else: ?>
                                     <a href="<?php echo URLROOT; ?>/Pages/AuthLogin"
                                         class="block hover:bg-gray-100 px-4 py-2 text-sm text-gray-700">Login</a>
@@ -82,15 +94,3 @@
         </div>
     </nav>
 
-
-
-
-
-
-
-
-
-
-</body>
-
-</html>
